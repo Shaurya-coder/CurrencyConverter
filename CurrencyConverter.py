1f0b25babf82afdbe90e8632a9050696e27711ce
Shaurya-coder <70806988+Shaurya-coder@users.noreply.github.com>
2020-10-23 19:24:49 +0530

    Update CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,20 +1,20 @@
 ####### Code Starts Here ########
 #Step1
 # Usual importing tkinter and other modules(forex-python for this project only),mainloop and defining app window default width and height,etc.
-# from forex_python.converter import CurrencyRates #if you want to use forex python for rates
+# from forex_python.converter import CurrencyRates#pip install forex-python #if you want to use forex python for rates
 from tkinter import *
-import requests #If you want to make a direct requests to the api for rates
+import requests#pip install requests #If you want to make a direct requests to the api for rates
 import json#To convert json to dictionary in Python
 root = Tk()#root is the name of the main window you caan also name it main,master,window1 etc
 
-root.title("Currency Converter")
+root.title("Currency Converter")#Title of Window
 
-root.geometry('450x400')
+root.geometry('450x400')#Dimensions of Window
 
 #Step2
 # Writing Title Label and other labels in window to serve as a reference to textbox.
-Title_label=Label(root,text = "Currency Converter",font = 'comicsansms 30 bold',pady = 5)#.grid(row = 0,column = 1 )
-Title_label.grid(row = 0,column = 1)
+Title_label=Label(root,text = "Currency Converter",font = 'comicsansms 30 bold',pady = 5)#Making a Label#.grid(row = 0,column = 1 )
+Title_label.grid(row = 0,column = 1)#griding the label to a place
 
 From_currency = Label(root,text = "From",font = 'size 15 ',pady = 5)
 From_currency.grid()
@@ -30,11 +30,11 @@ Amount.grid()
 # Defining the type of input variable.
 
 From_value = StringVar()
-From_value.set('USD')
+From_value.set('USD')#Setting default Dropdown Currency
 
 To_value = StringVar()
-To_value.set('INR')
-Amount_value = IntVar()
+To_value.set('INR')#Setting default Dropdown Currency
+Amount_value = IntVar()#Getting the quantity of currency to be converted
 
 
 
@@ -120,7 +120,7 @@ def convert():
     Response_label.destroy()
     # If you want to do through requests module.:-
     #Step I
-    # Making Get requests
+    # Making Get requests to the api
     r = requests.get(f"https://api.ratesapi.io/api/latest?base={From_value.get()}&symbols={To_value.get()}")
     #Step II
     # Getting the content from the page
@@ -154,6 +154,6 @@ def convert():
 Convert_Button = Button(text = "Convert",bg = 'white',fg = 'black',font = "size 15",pady = 0,command = convert)
 Convert_Button.grid(column = 1,rows = 5)
 
-root.mainloop()
+root.mainloop()#Makes a loop which keeps on running unti the user clickes the cross button on the window
 
 ####### Code Finishes Here ########
