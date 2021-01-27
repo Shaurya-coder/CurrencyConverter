dba328aca1278befc91f9e0f14fd9e4bee77d586
Shaurya-coder <70806988+Shaurya-coder@users.noreply.github.com>
2021-01-27 14:11:20 +0530

    Update CurrencyConverter.py
    Added Copy button in GUI

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,10 +1,20 @@
+"""
+Goal:-
+To make a currency converter 
+desktop app that uses an api
+
+Prerequisites:-
+1. Basic python knowledge (variables, data types, conditionals, loops, functions, etc)
+2. Basic tkinter knowledge (required only for the graphic user interface part)
+"""
 ####### Code Starts Here ########
 #Step1
 # Usual importing tkinter and other modules(forex-python for this project only),mainloop and defining app window default width and height,etc.
 # from forex_python.converter import CurrencyRates#pip install forex-python #if you want to use forex python for rates
 from tkinter import *
-import requests#pip install requests #If you want to make a direct requests to the api for rates
 import json#To convert json to dictionary in Python
+import requests#pip install requests #If you want to make a direct requests to the api for rates
+import pyperclip# pip install pyperclip # to make the copy button
 root = Tk()#root is the name of the main window you caan also name it main,master,window1 etc
 
 root.title("Currency Converter")#Title of Window
@@ -103,9 +113,9 @@ Currencies2.grid(column = 2,row = 2)
 #Step5
 # Making a textbox.
 
-From_entry = Entry(root,textvariable = From_value)
-To_entry = Entry(root,textvariable = To_value)
-Amount_entry = Entry(root,textvariable = Amount_value)
+From_entry = Entry(root,textvariable = From_value,bd = 0)
+To_entry = Entry(root,textvariable = To_value,bd = 0)
+Amount_entry = Entry(root,textvariable = Amount_value,bd = 0)
 
 
 #Step6
@@ -117,7 +127,10 @@ Amount_entry.grid(column = 1,row = 3)
 #Step7
 # Making a function to convert the currency.
 Response_label=Label(root)#Empty label 
+rates = None
+
 def convert():
+    global rates
     global Response_label
     Response_label.destroy()
     # If you want to do through requests module.:-
@@ -151,10 +164,23 @@ def convert():
     Response_label.grid(row = 20,column = 1)
 
 #Step8
+# Making a copy function to copy the converted amount
+
+def copy_function():
+    global rates,To_value
+    pyperclip.copy(f'{rates} {To_value.get()}')
+
+#Step9
 # Making a button and inserting the convert function into it. 
 
 Convert_Button = Button(text = "Convert",bg = 'sky blue',fg = 'black',font = "size 15",pady = 0,command = convert)
 Convert_Button.grid(column = 1,rows = 5)
+
+#Step10
+# Making a Copy Button to copy the converted amount
+
+Copy_button = Button(root,text = "Copy",bg = 'sky blue',fg = 'black',font = "size 15",pady = 0,command = copy_function)
+Copy_button.grid(column = 2,rows = 20)
 
 root.mainloop()#Makes a loop which keeps on executing the above program until the user clickes the cross button on the window
 
